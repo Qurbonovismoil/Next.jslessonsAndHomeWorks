c95d7b7f419c3310065d12cf30322c4e20df3dc5
Qurbonovismoil <ismoil.o6@icloud.com>
2026-05-07 17:58:12 +0500

    Сохранение моего кода

---
+++ i18n/request.ts
@@ -0,0 +1,14 @@
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
+const locales = ['en', 'ru', 'tj'];
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
